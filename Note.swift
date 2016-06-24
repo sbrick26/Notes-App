3426b356ef2e5dee8d8b348d6543e04dd5187b79
sbrick26 <swayambarik@gmail.com>
2016-06-24 10:20:50 -0700

    Completed Notes App

---
+++ Note.swift
@@ -0,0 +1,17 @@
+//
+//  Note.swift
+//  MakeSchoolNotes
+//
+//  Created by Swayam Barik on 6/22/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Note: Object
+{
+    dynamic var title = ""
+    dynamic var content = ""
+    dynamic var modificationTime = NSDate()
+}
